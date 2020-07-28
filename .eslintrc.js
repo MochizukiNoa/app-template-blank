41b3f5081b62ac56a819e5f0fb3528229961cef1
hrdtbs <harada.tsubasa.0422@gmail.com>
2020-07-28 20:52:06 +0900

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+/** @type {import('eslint').CLIEngine.Options} */
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 9,
+    sourceType: 'module',
+  },
+  env: {
+    es6: true,
+    browser: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  plugins: ['sort-imports-es6-autofix'],
+  rules: {
+    'sort-imports-es6-autofix/sort-imports-es6': [
+      2,
+      {
+        ignoreCase: false,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+      },
+    ],
+  },
+  settings: {
+    node: {
+      tryExtensions: ['.js'],
+    },
+  },
+  overrides: [
+    {
+      files: ['*.js', '*.config.js', 'config.js'],
+      env: {
+        node: true,
+      },
+    },
+  ],
+};
